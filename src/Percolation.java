9b24cb11a94a109a416e22d1565696f19d92b640
Hizkias Abraham <hizkiasnew@gmail.com>
2019-08-23 23:07:42 +0300

    cleaned up

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,12 +1,7 @@
-import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-
-import javax.sound.midi.Soundbank;
-
-
 public class Percolation {
     // Weighted quick union find
-    WeightedQuickUnionUF weightedUF;
+    private WeightedQuickUnionUF weightedUF;
     // Sites
     private int[][] sites;
     // open or closed site track
@@ -19,11 +14,8 @@ public class Percolation {
     private int topVirtualSite;
     // bottom virtual site
     private  int bottomVirtualSite;
-
-
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n){
-
         if(n <= 0){
             throw  new IllegalArgumentException("N must be greater than 0");
         }
@@ -43,13 +35,10 @@ public class Percolation {
                 }
             }
         }
-
     }
-
     // opens the site (row, col) if it is not open already
     // It will also connect to any neighbour site that is already open
     public void open(int row, int col){
-
         if(row <= 0 || col <= 0){
             throw new IllegalArgumentException("Row and column must be greater than 0");
         }
@@ -59,7 +48,6 @@ public class Percolation {
         else {
             row = row - 1;
             col = col -1;
-
             // check if the selected site resides on the side of the grid
             int[][] neighbours;
             /*
@@ -67,40 +55,30 @@ public class Percolation {
                 the site is not a corner
             */
             if(row == 0){
-
                 // The first corner at sites[0][0]
-
                 if(col == 0){
                     // It has always 2 neighbours
                     neighbours = new int[2][2];
-
                     // The right side neighbour
                     neighbours[0][0] = row;
                     neighbours[0][1] = col + 1;
                     // The bottom neighbour
                     neighbours[1][0] = row + 1;
                     neighbours[1][1] = col;
-
                 }
-
                 // the second corner at [0][sizeOfN - 1], it has always 2 neighbours
                 else if(col == sizeOfN -1){
-
                     neighbours = new int[2][2];
-
                     // the left side neighbour
                     neighbours[0][0] = row;
                     neighbours[0][1] = col - 1;
                     // the bottom neighbour
                     neighbours[1][0] = row + 1;
                     neighbours[1][1] = col;
-
                 }
-
                 // all other top row items row[0][col], they have always 3 neighbours
                 else {
                     neighbours = new int[3][2];
-
                     // the left side neighbour
                     neighbours[0][0] = row;
                     neighbours[0][1] = col - 1;
@@ -111,19 +89,13 @@ public class Percolation {
                     neighbours[2][0] = row + 1;
                     neighbours[2][1] = col;
                 }
-
                 weightedUF.union(sites[row][col], topVirtualSite);
-
             }
-
             // open from the bottom row
-
             else if(row == sizeOfN -1){
-
                 // the third corner has always only 2 neighbours
                 if(col == 0){
                     neighbours = new int[2][2];
-
                     // the top neighbour it has always 2 neighbours
                     neighbours[0][0] = row - 1;
                     neighbours[0][1] = col;
@@ -134,7 +106,6 @@ public class Percolation {
                 // the fourth corner
                 else if( col == sizeOfN - 1){
                     neighbours = new int[2][2];
-
                     // the top neighbour it has always 2 neighbours
                     neighbours[0][0] = row -1;
                     neighbours[0][1] = col;
@@ -142,11 +113,9 @@ public class Percolation {
                     neighbours[1][0] = row;
                     neighbours[1][1] = col - 1;
                 }
-
                 // all other rows from the bottom row. The always have 3 neighbours
                 else {
                     neighbours = new int[3][2];
-
                     // the top neighbour
                     neighbours[0][0] = row - 1;
                     neighbours[0][1] = col;
@@ -156,19 +125,12 @@ public class Percolation {
                     // the right side neighbour
                     neighbours[2][0] = row;
                     neighbours[2][1] = col + 1;
-
                 }
-
                 weightedUF.union(sites[row][col], bottomVirtualSite);
-
             }
-
             // open from the left side except the corner sites (already addressed above) they always have 3 neighbors
-
             else if(col == 0){
-
                 neighbours = new int[3][2];
-
                 // the top neighbour
                 neighbours[0][0] = row - 1;
                 neighbours[0][1] = col;
@@ -178,13 +140,10 @@ public class Percolation {
                 // the bottom neighbour
                 neighbours[2][0] = row + 1;
                 neighbours[2][1] = col;
-
             }
-
             // open from the right side sites, except the corner ones(addressed above). They always have 3 neighbours
             else if(col == sizeOfN -1){
                 neighbours = new int[3][2];
-
                 // the top neighbour
                 neighbours[0][0] = row - 1;
                 neighbours[0][1] = col;
@@ -196,10 +155,8 @@ public class Percolation {
                 neighbours[2][1] = col - 1;
             }
             // open from any other internal sites, each has always four neighbours
-
             else {
                 neighbours = new int[4][2];
-
                 // the top neighbour
                 neighbours[0][0] = row -1;
                 neighbours[0][1] = col;
@@ -212,26 +169,22 @@ public class Percolation {
                 // the right side neighbour
                 neighbours[3][0] = row;
                 neighbours[3][1] = col + 1;
-
             }
             // Set the open blocked flag of the entry true
-            openOrBlocked[row][col] = true;
-            countOpenSites += 1;
-
+           if(!isOpen(row + 1, col + 1)){
+               openOrBlocked[row][col] = true;
+               countOpenSites += 1;
+           }
             // to check whether a neighbour is open or closed, if open, connected or not and to union if it is 
             // open but yet not unioned together. loops maximum of constant 4
-//            System.out.println(row+ ", "+col+" has "+neighbours.length+" neighbours ");
             for (int i = 0; i < neighbours.length; i++) {
                 int currentSite = sites[row][col];
              if(isOpen(neighbours[i][0] + 1, neighbours[i][1] + 1) && !weightedUF.connected(currentSite, sites[neighbours[i][0]][neighbours[i][1]])){
                      weightedUF.union(currentSite, sites[neighbours[i][0]][neighbours[i][1]]);
-
              }
             }
         }
-
     }
-
     // is the site (row, col) open?
     public boolean isOpen(int row, int col){
         if(row <= 0 || col <= 0){
@@ -241,46 +194,22 @@ public class Percolation {
             throw new IllegalArgumentException("Row and coulumn size must be less than N");
         }
         else {
-//            System.out.println("checking "+row+", "+col+" "+openOrBlocked[row - 1][col -1 ]);
             return  openOrBlocked[row - 1][col - 1];
         }
     }
-
     // is the site (row, col) full?
     public boolean isFull(int row, int col){
         return weightedUF.connected(sites[row - 1][col - 1], topVirtualSite);
     }
-
     // returns the number of open sites
     public int numberOfOpenSites(){
         return  countOpenSites;
     }
-
     // does the system percolate?
     public boolean percolates(){
-
         return  weightedUF.connected(topVirtualSite, bottomVirtualSite);
     }
-
     // test client (optional)
     public static void main(String[] args){
-
-//        Percolation percolation = new Percolation(4);
-//
-////        System.out.println("Does percolate "+percolation.percolates());
-//        percolation.open(1, 1);
-//        percolation.open(2,1);
-//        percolation.open(3,2);
-//        percolation.open(3,1);
-//        percolation.open(3,3);
-//        percolation.open(4,3);
-//        percolation.open(4,4);
-////        percolation.open(4,3);
-////        System.out.println("Is full 2, 2 "+percolation.isFull(2, 2));
-//        System.out.println("Is full 4, 4"+ percolation.isFull(4, 4));
-//        System.out.println("Number of open sites "+percolation.numberOfOpenSites());
-//        System.out.println("Does percolate "+percolation.percolates());
-
     }
-
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,62 +1,58 @@
 import edu.princeton.cs.algs4.StdRandom;
-
-
-
 public class PercolationStats {
-
-    Percolation percolation;
-    int[] thresholds;
+    private Percolation percolation;
+    private double mean;
+    private double stddev;
+    private double confidenceLow;
+    private double confidenceHigh;
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials){
-        thresholds = new int[trials];
-
+        double[] thresholds = new double[trials];
+        double sumThresholds = 0.0;
         for (int i = 0; i < trials; i++) {
             percolation = new Percolation(n);
             while (!percolation.percolates()){
                 int row = StdRandom.uniform(1, n+1);
                 int col = StdRandom.uniform(1, n+1);
                 percolation.open(row, col);
-                System.out.println("Opening ["+row+"]"+"["+col+"]");
             }
-            thresholds[i] = percolation.numberOfOpenSites();
-            System.out.println("Percolated "+percolation.numberOfOpenSites());
+            double currentThreshold = ((double)percolation.numberOfOpenSites()/(double)(n * n));
+            sumThresholds += currentThreshold;
+            thresholds[i] = currentThreshold;
         }
+        // mean of thresholds
+        mean = sumThresholds / (double)trials;
+        // sum of squared mean difference
+        double sqMean = 0.0;
+        for (int i = 0; i < trials; i++) {
+            sqMean += Math.pow((thresholds[i] - mean), 2);
+        }
+        stddev = Math.pow((sqMean/((double)trials - 1)), 0.5);
+        confidenceLow = mean - (1.96*stddev/(Math.pow(trials, 0.5)));
+        confidenceHigh = mean + (1.96*stddev/(Math.pow(trials, 0.5)));
     }
-
     // sample mean of percolation threshold
     public double mean(){
-            int sum = 0;
-        for (int i = 0; i < thresholds.length; i++) {
-            sum += thresholds[i];
-        }
-
-        return sum/thresholds.length;
+        return mean;
     }
-
     // sample standard deviation of percolation threshold
     public double stddev(){
-        double squaredMean = 0.0;
-        for (int i = 0; i < thresholds.length; i++) {
-            squaredMean += Math.pow((thresholds[i] - mean()), 2);
-        }
-
-        return  squaredMean/(thresholds.length - 1);
+        return  stddev;
     }
-
     // low endpoint of 95% confidence interval
     public double confidenceLo(){
-        return  0.0;
+        return  confidenceLow;
     }
-
     // high endpoint of 95% confidence interval
     public double confidenceHi(){
-    return  0.0;
-    }
 
+        return  confidenceHigh;
+    }
     // test client (see below)
     public static void main(String[] args){
         PercolationStats pstat = new PercolationStats(200, 100);
-        System.out.println("Mean "+pstat.mean());
+        System.out.println("mean                    = "+pstat.mean());
+        System.out.println("stddev                  = "+pstat.stddev());
+        System.out.println("95% confidence interval = ["+pstat.confidenceLo()+", "+pstat.confidenceHi()+"]");
     }
-
 }
